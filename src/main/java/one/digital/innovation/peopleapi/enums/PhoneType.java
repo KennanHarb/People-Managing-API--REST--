6ca02b094e5a56267731656de019cfa1d25f8d32
Kennan Harb <73542036+KennanHarb@users.noreply.github.com>
2021-07-15 10:51:05 -0300

    1.02

--- src/main/java/one/digital/innovation/peopleapi/enums/PhoneType.java
+++ src/main/java/one/digital/innovation/peopleapi/enums/PhoneType.java
@@ -6,5 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum PhoneType {
-    HOME, COMMERCIAL, MOBILE
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
 }
